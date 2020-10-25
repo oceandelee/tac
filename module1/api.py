2ef160b037027d6b1925d3c274028db92c685461
oceandelee <72808373+oceandelee@users.noreply.github.com>
2020-10-25 15:52:29 +0100

    Add files via upload
    Ajout API (pour OpenData) et SPARQL (pour la requête des prix reçus par des personnes nées à Bruxelles)

---
+++ module1/api.py
@@ -0,0 +1,52 @@
+"""API for AVB"""
+
+import json
+import sys
+
+import requests
+
+
+
+def actualite_found ():
+    osm = "https://opendata.bruxelles.be/api/datasets/1.0/search/?q="
+    data = {
+
+    "nhits":0,
+    "parameters":{
+        "dataset":"actualites-ville-de-bruxelles",
+        "timezone":"UTC",
+        "q":"actualite",
+        "language": "fr",
+        "rows":10,
+        "start":0,
+        "sort":[
+            "published"
+        ]
+        ,
+        "format":"json"
+    }
+    ,
+    "records":[]
+    }
+    resp = requests.get(osm, data)
+    if resp.status_code == 200:
+        print(resp.json()["datasets"][0]["metas"])
+    else:
+        print("actualite not found")
+    return resp
+
+def get_result(resp,n,attribut):
+    metas = resp.json()["datasets"][n]["metas"]
+    return metas[attribut]
+
+
+
+def nb_result(resp):
+    return len(resp.json()["datasets"])
+
+#Example of use
+if __name__ == "__main__":
+    resp = actualite_found()
+    result = get_result(resp,2,"description")
+    print(result)
+    print(nb_result(resp))
